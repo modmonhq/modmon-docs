93d0c502dac98328354bafb1b0df2803c70f44e6
Mason Curry <mason.curry1@gmail.com>
2025-07-19 11:30:48 -0500

    feat: further config updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -83,9 +83,8 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
