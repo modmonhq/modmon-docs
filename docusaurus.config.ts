bf06de8ac3f0fc6664fd378154185f77b56c03de
Mason Curry <mason.curry1@gmail.com>
2025-07-20 21:52:07 -0500

    feat: add api docs demo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,161 +1,292 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Modmon Developer Site',
-  tagline: 'Simplifying deployment of modular monolith applications',
-  favicon: 'img/favicon.ico',
+	title: 'Modmon Developer Site',
+	tagline: 'Simplifying deployment of modular monolith applications',
+	favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+	// Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+	future: {
+		v4: true, // Improve compatibility with the upcoming Docusaurus v4
+	},
 
-  // Set the production url of your site here
-  url: 'https://modmon.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: 'https://modmon.dev',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'modmonhq', // Usually your GitHub org/user name.
-  projectName: 'modmon', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'modmonhq', // Usually your GitHub org/user name.
+	projectName: 'modmon', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
+	presets: [
+		[
+			'classic',
+			{
+				docs: {
+					sidebarPath: './sidebars.ts',
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						'https://github.com/modmonhq/modmon-docs/tree/master/',
+          docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ['rss', 'atom'],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						'https://github.com/modmonhq/modmon-docs/tree/master/',
+					// Useful options to enforce blogging best practices
+					onInlineTags: 'warn',
+					onInlineAuthors: 'warn',
+					onUntruncatedBlogPosts: 'warn',
+				},
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			} satisfies Preset.Options,
+		],
+	],
+
+	themeConfig: {
+		// Replace with your project's social card
+		image: 'img/docusaurus-social-card.jpg',
+		navbar: {
+			title: 'Modmon for Developers',
+			logo: {
+				alt: 'Modmon Logo',
+				src: 'img/logo.svg',
+			},
+			items: [
+				{
+					type: 'docSidebar',
+					sidebarId: 'tutorialSidebar',
+					position: 'left',
+					label: 'Docs',
+				},
+				{
+          label: 'API Docs',
+					position: 'left',
+					to: "/docs/category/petstore-api",
+				},
+				{ to: '#', label: 'Changelog', position: 'left' },
+				{
+					href: 'https://github.com/modmonhq/modmon',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'Documentation',
+					items: [
+						{
+							label: 'Docs',
+							to: '/docs/intro',
+						},
+						{
+							label: 'API Docs',
+							to: '#',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'GitHub Discussions',
+							href: 'https://github.com/modmonhq/modmon/discussions',
+						},
+						{
+							label: 'Discord',
+							href: 'https://discord.gg/YVsfzBf4Jt',
+						},
+						{
+							label: 'X',
+							href: '#',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'Website',
+							to: 'https://modmon.org',
+						},
+						{
+							label: 'Blog',
+							to: 'https://blog.modmon.org',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/modmonhq',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Modmon Foundation. Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+			additionalLanguages: [
+				'ruby',
+				'csharp',
+				'php',
+				'java',
+				'powershell',
+				'json',
+				'bash',
+				'dart',
+				'objectivec',
+				'r',
+			],
+		},
+		languageTabs: [
+			{
+				highlight: 'python',
+				language: 'python',
+				logoClass: 'python',
+			},
+			{
+				highlight: 'bash',
+				language: 'curl',
+				logoClass: 'curl',
+			},
+			{
+				highlight: 'csharp',
+				language: 'csharp',
+				logoClass: 'csharp',
+			},
+			{
+				highlight: 'go',
+				language: 'go',
+				logoClass: 'go',
+			},
+			{
+				highlight: 'javascript',
+				language: 'nodejs',
+				logoClass: 'nodejs',
+			},
+			{
+				highlight: 'ruby',
+				language: 'ruby',
+				logoClass: 'ruby',
+			},
+			{
+				highlight: 'php',
+				language: 'php',
+				logoClass: 'php',
+			},
+			{
+				highlight: 'java',
+				language: 'java',
+				logoClass: 'java',
+				variant: 'unirest',
+			},
+			{
+				highlight: 'powershell',
+				language: 'powershell',
+				logoClass: 'powershell',
+			},
+			{
+				highlight: 'dart',
+				language: 'dart',
+				logoClass: 'dart',
+			},
+			{
+				highlight: 'javascript',
+				language: 'javascript',
+				logoClass: 'javascript',
+			},
+			{
+				highlight: 'c',
+				language: 'c',
+				logoClass: 'c',
+			},
+			{
+				highlight: 'objective-c',
+				language: 'objective-c',
+				logoClass: 'objective-c',
+			},
+			{
+				highlight: 'ocaml',
+				language: 'ocaml',
+				logoClass: 'ocaml',
+			},
+			{
+				highlight: 'r',
+				language: 'r',
+				logoClass: 'r',
+			},
+			{
+				highlight: 'swift',
+				language: 'swift',
+				logoClass: 'swift',
+			},
+			{
+				highlight: 'kotlin',
+				language: 'kotlin',
+				logoClass: 'kotlin',
+			},
+			{
+				highlight: 'rust',
+				language: 'rust',
+				logoClass: 'rust',
+			},
+		],
+	} satisfies Preset.ThemeConfig,
+  
+  plugins: [
     [
-      'classic',
+      "docusaurus-plugin-openapi-docs",
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/modmonhq/modmon-docs/tree/master/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/modmonhq/modmon-docs/tree/master/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          petstore: {
+            specPath: "examples/petstore.yaml",
+            outputDir: "docs/petstore",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag",
+            },
+          } satisfies OpenApiPlugin.Options,
+        } satisfies Plugin.PluginOptions,
+      },
     ],
   ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Modmon for Developers',
-      logo: {
-        alt: 'Modmon Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
-        },
-        {
-          to: '#',
-          label: 'API Docs',
-          position: 'left',
-        },
-        {to: '#', label: 'Changelog', position: 'left'},
-        {
-          href: 'https://github.com/modmonhq/modmon',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Documentation',
-          items: [
-            {
-              label: 'Docs',
-              to: '/docs/intro',
-            },
-            {
-              label: 'API Docs',
-              to: '#',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'GitHub Discussions',
-              href: 'https://github.com/modmonhq/modmon/discussions',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/YVsfzBf4Jt',
-            },
-            {
-              label: 'X',
-              href: '#',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Website',
-              to: 'https://modmon.org',
-            },
-            {
-              label: 'Blog',
-              to: 'https://blog.modmon.org',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/modmonhq',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Modmon Foundation. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+  themes: ["docusaurus-theme-openapi-docs"],
 };
 
 export default config;
